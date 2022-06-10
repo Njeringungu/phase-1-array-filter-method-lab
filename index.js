ef28bf254d9348e3227100936594550c0981f684
Janet Njeri Ngungu <janetnjerin89@gmail.com>
2022-06-11 01:44:17 +0300

    adding filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(driver, sort) {
+    return driver.filter(
+      possibleMatch => possibleMatch.toLowerCase() === sort.toLowerCase()
+    );
+  }
+  const fuzzyMatch = (driver, testString) => {
+    return driver.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  const matchName = (driver, sortName) => {
+    return driver.filter((record) => record.name === sortName);
+  }
